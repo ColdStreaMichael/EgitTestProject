77fa5cd531b9a01e1a944a89a4553e5dda3b7d25
heyuguan <448024090@qq.com>
2017-09-11 19:59:23 +0800

    zx

---
+++ TestEgit.java
@@ -0,0 +1,16 @@
+package com.atguigu.egit;
+
+public class TestEgit {
+
+	public static void main(String[] args) {
+		System.out.println("Testing Egit");
+
+		System.out.println("Testing Egit");
+		System.out.println("Testing Egit");
+		System.out.println("Testing Egit");
+		System.out.println("Testing Egit");
+		System.out.println("Testing Egit");
+		System.out.println("Testing Egit");
+	}
+
+}
